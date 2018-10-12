e7e2e1619bf26a1db116be8b11e2d98fa6d29486
Mike S <mstange@dthera.com>
2018-10-12 14:26:03 -0700

    added node_modules to gitignore

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,193 +1,149 @@
-const connection = require("./Connection");
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
+const connection = require('./Connection');
 
-var queryString = "";
+let queryString = '';
+let lineBreak = '';
 
-var lineBreak = "";
-
-for(i = 0; i < 100; i++) {
-    lineBreak += "-";
+for (let i = 0; i < 100; i += 1) {
+  lineBreak += '-';
 }
 
 process.stdout.write('\x1B[2J\x1B[0f');
-console.log("===========================");
-console.log("Welcome to Bamazon Shopping")
-console.log("===========================");
+console.log('===========================');
+console.log('Welcome to Bamazon Shopping');
+console.log('===========================');
 
 shopBamazon();
 
 function shopBamazon() {
+  let tempProduct;
+  queryString = 'SELECT * FROM products';
 
-    var tempProduct;
-    queryString = "SELECT * FROM products";
+  connection.query(queryString, (error, response) => {
+    if (error) throw error;
+    displayProducts(response);
 
-    connection.query(queryString, function(error, response) {
+    console.log('What would you like to buy?');
 
-        if(error) throw error;
+    inquirer.prompt([{
+      message: 'Select an item by ID (q to quit)',
+      name: 'updateID',
+    }])
+      .then((answer1) => {
+        if (answer1.updateID.toLowerCase() === 'q') {
+          connection.end();
+          return;
+        }
 
-        displayProducts(response);
+        // loop through response array, searching for a match on ID
+        for (let i = 0; i < response.length; i += 1) {
+          if (response[i].id === parseInt(answer1.updateID, 10)) {
+            tempProduct = response[i];
+            break;
+          }
+        }
 
-        console.log("What would you like to buy?");
-
-        inquirer.prompt([
-            {
-                message: "Select an item by ID (q to quit)",
-                name: "updateID"
-            }
-        ]).then(function(answer1) {    
-
-            if(answer1.updateID.toLowerCase() === "q") {
-                
-                connection.end();
-                return;
-            }
-
-            // loop through response array, searching for a match on ID
-            for(i = 0; i < response.length; i++) {
-                
-                if(response[i].id === parseInt(answer1.updateID)) {
-
-                    tempProduct = response[i];
-                    break;
-                }
-            }
-                
-            // if tempProduct was not assigned, invalid input
-            if(!tempProduct) {
-
+        // if tempProduct was not assigned, invalid input
+        if (!tempProduct) {
+          process.stdout.write('\x1B[2J\x1B[0f');
+          console.log(lineBreak);
+          console.log('Invalid input.  Try Again.');
+          console.log(lineBreak);
+          shopBamazon();
+        } else {
+          inquirer.prompt([{
+            message: 'How many would you like to buy?',
+            type: 'input',
+            name: 'quantity',
+          }])
+            .then((answers) => {
+              if (!parseInt(answers.quantity, 10)) {
                 process.stdout.write('\x1B[2J\x1B[0f');
-                console.log(lineBreak);
-                console.log("Invalid input.  Try Again.");
-                console.log(lineBreak);
+                console.log('====================');
+                console.log('Error: Invalid Input');
+                console.log('====================');
                 shopBamazon();
-            }
+              } else if (tempProduct.stock_quantity < answers.quantity) {
+                process.stdout.write('\x1B[2J\x1B[0f');
+                console.log('/======================');
+                console.log('Insufficient quantity!');
+                console.log('======================');
+                shopBamazon();
+              } else {
+                queryString = `UPDATE products SET stock_quantity = stock_quantity - ${answers.quantity} WHERE id=${tempProduct.id.toString()}`;
 
-            else {
+                const totalSale = (parseInt(answers.quantity, 10) * tempProduct.price);
 
-                inquirer.prompt([
-                    {
-                        message: "How many would you like to buy?",
-                        type: "input",
-                        name: "quantity"
-                    }     
-                ]).then(function(answers){
+                connection.query(queryString, () => {
+                  console.log(lineBreak);
+                  console.log(`Item purchased: ${answers.quantity} ${tempProduct.product_name}`);
+                  console.log(`Total price: $${totalSale.toFixed(2)}`);
+                  console.log(lineBreak);
 
-                    if(!parseInt(answers.quantity)) {
+                  queryString = `UPDATE products SET product_sales = product_sales + ${totalSale.toFixed()} WHERE id=${tempProduct.id.toString()}`;
 
-                        process.stdout.write('\x1B[2J\x1B[0f');
-                        console.log("====================");
-                        console.log("Error: Invalid Input");
-                        console.log("====================");
-                        shopBamazon();
-                    }
-
-                    // check to see if there's sufficient quantity for order
-                    else if(tempProduct.stock_quantity < answers.quantity) {
-
-                        process.stdout.write('\x1B[2J\x1B[0f');
-                        console.log("/======================");
-                        console.log("Insufficient quantity!");
-                        console.log("======================");
-                        shopBamazon();
-                    }
-                
-                    else {
-
-                        // update item in database
-                        queryString = "UPDATE products SET stock_quantity = stock_quantity - " +
-                                            answers.quantity + " WHERE id=" + tempProduct.id.toString();
-
-                        var totalSale = (parseInt(answers.quantity) * tempProduct.price);
-                        connection.query(queryString, function(error, response) {
-
-                            console.log(lineBreak);
-                            console.log("Item purchased: " + answers.quantity + " " + tempProduct.product_name);
-                            console.log("Total price: $" + totalSale.toFixed(2));
-                            console.log(lineBreak);
-
-                            
-                            queryString = "UPDATE products SET product_sales = product_sales + " +
-                            totalSale.toFixed() + " WHERE id=" + tempProduct.id.toString();
-
-                            connection.query(queryString, function(error, response) {
-
-                                if(error) console.log(error);
-                                
-                                continuePrompt();
-                            });
-                        });
-                    }
-                });             
-            }
-        });
-    });
+                  connection.query(queryString, (err) => {
+                    if (err) console.log(err);
+                    continuePrompt();
+                  });
+                });
+              }
+            });
+        }
+      });
+  });
 }
 
 function displayProducts(response) {
-    
-   
-    var displayNames = [];
-    var displayDepartments = [];
-    var displayPrices = [];
+  const displayNames = [];
+  const displayDepartments = [];
+  const displayPrices = [];
 
-    console.log("\n ID | Item Name                                | Department           | Price" +
-                                                    "       | Stock");
-    console.log(lineBreak);
+  console.log('\n ID | Item Name                                | Department           | Price       | Stock');
+  console.log(lineBreak);
 
-    for(var i = 0; i < response.length; i++) {
+  for (let i = 0; i < response.length; i += 1) {
+    displayNames.push(response[i].product_name);
+    displayDepartments.push(response[i].department_name);
+    displayPrices.push(response[i].price.toFixed(2).toString());
 
-        displayNames.push(response[i].product_name);
-        displayDepartments.push(response[i].department_name);
-        displayPrices.push(response[i].price.toFixed(2).toString());
-
-        // add space to product_name for display
-        while(displayNames[i].length < 40) {
-            displayNames[i] += " ";
-        }
-        // add space to product_name for display
-        while(displayDepartments[i].length < 20) {
-            displayDepartments[i] += " ";
-        }
-        // add space to product_name for display
-        while(displayPrices[i].length < 10) {
-            displayPrices[i] += " ";
-        }
-
-        // add a space before display for single digit IDs
-        if(i < 9) {
-            console.log("  " + response[i].id + " | " + displayNames[i] + " | " +
-                            displayDepartments[i] + " | $" + displayPrices[i] +
-                             " | " + response[i].stock_quantity);
-        } else {
-            console.log(" " + response[i].id + " | " + displayNames[i] + " | " +
-                            displayDepartments[i] + " | $" + displayPrices[i] +
-                            " | " + response[i].stock_quantity);
-        }
+    // add space to product_name for display
+    while (displayNames[i].length < 40) {
+      displayNames[i] += ' ';
     }
-    console.log(lineBreak);    
+    // add space to product_name for display
+    while (displayDepartments[i].length < 20) {
+      displayDepartments[i] += ' ';
+    }
+    // add space to product_name for display
+    while (displayPrices[i].length < 10) {
+      displayPrices[i] += ' ';
+    }
+
+    // add a space before display for single digit IDs
+    if (i < 9) {
+      console.log(`  ${response[i].id} | ${displayNames[i]} | ${displayDepartments[i]} | $${displayPrices[i]} | ${response[i].stock_quantity}`);
+    } else {
+      console.log(` ${response[i].id} | ${displayNames[i]} | ${displayDepartments[i]} | $${displayPrices[i]} | ${response[i].stock_quantity}`);
+    }
+  }
+  console.log(lineBreak);
 }
 
 function continuePrompt() {
-    
-    inquirer.prompt([
-        {
-            message: "Continue shopping?",
-            type: "list",
-            choices: ["yes", "no"],
-            name: "again"
-        }
-    ]).then(function(answers) {
-    
-        if(answers.again.toLowerCase() === "yes") {
-
-            process.stdout.write('\x1B[2J\x1B[0f');
-            shopBamazon();
-        }
-
-        else {
-
-            console.log("Thank you for shopping at Bamazon! Goodbye.");
-            connection.end();
-        }
+  inquirer.prompt([{
+    message: 'Continue shopping?',
+    type: 'list',
+    choices: ['yes', 'no'],
+    name: 'again',
+  }])
+    .then((answers) => {
+      if (answers.again.toLowerCase() === 'yes') {
+        process.stdout.write('\x1B[2J\x1B[0f');
+        shopBamazon();
+      } else {
+        console.log('Thank you for shopping at Bamazon! Goodbye.');
+        connection.end();
+      }
     });
 }
